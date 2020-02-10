0c72aef7d60deb436ed45880456deeddeeac353a
Marco Antonio <marcoantoniocuberos@gmail.com>
2020-02-10 13:26:21 +0100

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Componente1Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
