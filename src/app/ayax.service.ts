6f5204fa81694317b2630c52439598430e9d6da7
Marcocuma <46998207+Marcocuma@users.noreply.github.com>
2020-02-21 19:49:49 +0100

    Solucion de errores

--- src/app/ayax.service.ts
+++ src/app/ayax.service.ts
@@ -1,37 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { promise } from 'protractor';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AyaxService {
   news = null;
-  page = 1;
   title = '';
   constructor(public http:HttpClient) {
-    this.http.get('https://newsapi.org/v2/everything?languaje=es&domains=elpais.com,elmundo.es,elcortodigital.es,ideal.es&page=1&apiKey=e859accb681646698b5ba6f1e8b23ba8').subscribe(
-      resultado => {
-        this.news = resultado
-      }
-    )
-  }
-  getNews(){
     
-    if(this.news != null)
-      return this.news.articles;
-    return null;
   }
-  getFiltrada(param:string){
-    this.page=1
+  public getNews<T>() : Observable <T>{
+    return this.http.get<T>('https://newsapi.org/v2/everything?languaje=es&domains=elpais.com,elmundo.es,elcortodigital.es,ideal.es&page=1&apiKey=e859accb681646698b5ba6f1e8b23ba8')
+  }
+  getFiltrada<T>(param:string): Observable <T>{
     console.log('Parametro'+param)
     var link='https://newsapi.org/v2/everything?languaje=es&qInTitle='+param
-    link+='&domains=elpais.com,elmundo.es,ideal.es&page='+this.page+'&apiKey=e859accb681646698b5ba6f1e8b23ba8'
-    this.http.get(link).subscribe(
-      resultado => {
-        this.news = resultado;
-      }
-    )
-    console.log(link)
-    return this.news.articles;
+    link+='&domains=elpais.com,elmundo.es,ideal.es&page=1&apiKey=e859accb681646698b5ba6f1e8b23ba8'
+    return this.http.get<T>(link)
+  }
+  cargarNoticias<T>(page = 1,filtro = null): Observable <T>{
+      var link;
+      if(filtro)
+        link='https://newsapi.org/v2/everything?languaje=es&qInTitle='+filtro
+      else
+        link='https://newsapi.org/v2/everything?languaje=es'
+      link+='&domains=elpais.com,elmundo.es,ideal.es&page='+page+'&apiKey=e859accb681646698b5ba6f1e8b23ba8'
+      return this.http.get<T>(link)
   }
 }
--- src/app/bienvenida/bienvenida.component.ts
+++ src/app/bienvenida/bienvenida.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AyaxService } from '../ayax.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-bienvenida',
@@ -10,18 +11,41 @@ export class BienvenidaComponent implements OnInit {
   importantes = []
   noticias = []
   valorFiltro = ''
+  bloqueo = false
+  page = 1
   constructor(public serv:AyaxService) { 
-    this.importantes = this.serv.news.slice(0,7)
-    this.noticias =  this.serv.news.slice(7, this.serv.news.length-1)
-    console.log(this.importantes)
-    console.log(this.noticias)
+    this.getNews()
+    $(window).scroll(() => {
+      if(!this.bloqueo&&this.page<6&&($(window).scrollTop() + $(window).height() >= $(document).height()-100)){
+        console.log("entra")
+        this.bloqueo = true
+        this.serv.cargarNoticias(this.page,this.valorFiltro).toPromise().then((result)=>{
+          this.anadirNoticias(result)
+          this.page++
+        }).finally(()=> this.bloqueo = false)
+      }
+    });
   }
 
   ngOnInit(): void {
   }
+  async getNews(){
+    this.serv.getNews().toPromise().then((resultado) => {
+      this.crearVariablesNoticias(resultado)
+      this.page++
+    })
+  }
+  anadirNoticias(resultado){
+    this.noticias = this.noticias.concat(resultado.articles)
+  }
+  crearVariablesNoticias(resultado){
+    this.importantes = resultado.articles.slice(0, 7);
+    this.noticias = resultado.articles.slice(7, resultado.length);
+  }
   filtrar(){
-    this.serv.title = this.valorFiltro
-    console.log('Filtro'+this.valorFiltro)
-    this.noticias=this.serv.getFiltrada(this.valorFiltro)
+    this.serv.getFiltrada(this.valorFiltro).toPromise().then((resultado) =>{
+      this.crearVariablesNoticias(resultado)
+      this.page = 2
+    })
   }
 }
